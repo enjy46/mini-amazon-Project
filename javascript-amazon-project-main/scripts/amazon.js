04a8570d0e5d5adb7b42350525839266af07e10f
enjy46 <enjyy.mowafy@gmail.com>
2025-04-24 11:53:37 +0200

    make the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -80,10 +80,20 @@ products.forEach((product) => {
 
         })
       }
-            console.log(cart);
+
+      let cartQuantity=0;
+
+      cart.forEach((item)=> {
+        cartQuantity+=item.quantity;                       
+      
+      
+    });
+
+            document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
 
       });
     });
+
 
   
   
