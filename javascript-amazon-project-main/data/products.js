78d95d4973c3aef60adb2c3504cc69528bc2aeb4
enjy46 <enjyy.mowafy@gmail.com>
2025-04-25 20:42:04 +0300

    load products from the backend

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -91,6 +91,32 @@ object3.method();
 
 */
 
+
+export let products = [];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () =>{
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+    fun();
+  })
+
+  xhr.open('GET', 'http://supersimplebackend.dev/products');
+  xhr.send();
+
+}
+loadProducts();
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -756,3 +782,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
